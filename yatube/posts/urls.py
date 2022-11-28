b9093fe7297957ab184297df13f9ea62025c74ec
Анатолий Терентьев <aterentv@mail.ru>
2022-11-28 21:51:58 +0500

    Added urls and view functions

---
+++ yatube/posts/urls.py
@@ -0,0 +1,9 @@
+"""yatube URL Configuration
+"""
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path('group/<slug:slug>/', views.group_posts),
+]
