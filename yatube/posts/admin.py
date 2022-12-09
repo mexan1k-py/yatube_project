ed791a1bf0a3bc84b0ccddd88125265d1c0ac888
mexan1k-py <aterentv@mail.ru>
2022-12-09 15:57:38 +0500

    new bd

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'text', 'pud_date', 'author')
+    search_fields = ('text',)
+    list_filter = ('pud_date',)
+    empty_value_display = '-пусто-'
+    
+admin.site.register(Post, PostAdmin)
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,19 @@
-from django.http import HttpResponse
-
+from django.shortcuts import render
+from .models import Post
 
 def index(request):
-    return HttpResponse('Главная страница')
+    title = 'Главная страница проекта "Yatube"'
+    posts =  Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'text': title,
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
 
-
-def group_posts(request, slug):
-    return HttpResponse('Фильтрация групп')
+def group_list(request):
+    template = 'posts/group_list.html'
+    title = 'Здесь будет информация о группах проекта "Yatube"'
+    context = {
+        'text': title,
+    }
+    return render(request, template, context)
